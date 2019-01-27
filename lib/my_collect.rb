eee92e9c0c2bc3c7ee5fc3a966c96c634d63d78d
Lu Daley <daleyluis@gmail.com>
2019-01-27 23:27:48 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,6 +3,14 @@
 #You are writing a method that behaves just like the real #collect method. It should take in an argument of a collection, iterate over that collection using a while loop, and execute the code in the block you call it with for each element in the collection (use the yield keyword). It should return the modified collection.
 
 #Your #my_collect method, therefore, should not care about the contents of the code block that it is invoked with. For example, let's say we are writing an app to help teachers manage their students. Our teacher has a list of students:
+
 def collect method(collection)
+
+#collection —> iterate over collection using a “while loop”
+            #"while loop” = “while n < 3 puts “not enough stars, keep trying”
+#execute code block that it is called with for each element using the “yield” keyword
+#should return modified collection
+#end
+
 
 end
