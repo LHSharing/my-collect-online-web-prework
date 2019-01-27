c4e736f827923ff5b9f0243db5f778fbf5597ed7
Lu Daley <daleyluis@gmail.com>
2019-01-27 23:47:31 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -6,8 +6,8 @@
 
 def my_collect(empty_array)
   counter = 0
+  new_array = []
   while counter < empty_array.length
-          new_array = []
           new_array << yield(empty_array[counter])
       counter += 1
   end
