d1fb40ddf5a3c9fa5e909ce86298ea8581efd30f
Lu Daley <daleyluis@gmail.com>
2019-01-27 22:48:47 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,8 @@
+#iterating over the given collection and passing each member to the given block––regardless of the content of the collection or the nature of the block.
 
+#You are writing a method that behaves just like the real #collect method. It should take in an argument of a collection, iterate over that collection using a while loop, and execute the code in the block you call it with for each element in the collection (use the yield keyword). It should return the modified collection.
 
+#Your #my_collect method, therefore, should not care about the contents of the code block that it is invoked with. For example, let's say we are writing an app to help teachers manage their students. Our teacher has a list of students:
+def collect method(collection)
+
+end
